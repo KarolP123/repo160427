1ea532f17d2731544a4968443f4cf981d256f606
KarolP123 <karolpartyka8@wp.pl>
2022-02-04 10:04:18 +0100

    version 1.1

--- mylna.cpp
+++ mylna.cpp
@@ -1,53 +1,66 @@
 #include<iostream>
+#include<cmath>
 
+using namespace std;
 //! Klasa MyLna */
 class MyLna
-    
- public:
-{
+{ 
+//! Zmienne publiczne */
+public:
+
     MyLna(double a = 0)  /*!<Metoda klasy MyLna */
     {
+        mA =a;
         
     }
     MyLna(double x, double a = 0) /*!<Metoda klasy MyLna */ 
     {
-        
+        mX = x;
+        mA = a;
     }
     
     MyLna(const MyLna &obj) /*!<Metoda klasy MyLna */ 
     {
-        
+        mX = obj.mX;
+        mA = obj.mA;
     }
     ~MyLna() // Destruktor klasy MyLna *!/
     {
-        
+      mX = 0;
+      mA = 0;
     }
     double value(double mX, double mA) /*<! Zwraca wartosc w otoczeniu punktu a */
     {
         
     }
-    void setX(double)
+    void setX(double) 
     {
-        
+       mX = x; 
     }
     double getX()
     {
-        
+        return mX; /*Zwraca mX */
     }
     void setA(double)
     {
-        
+        mA = a;
     }
     double getA()
     {
-        
+      return mA;  /* Zwraca mA */
     }
-    
+//! Zmienne prywatne */    
 private:
 double mX, /*<! double mX */
 double mA  /*<! double mA */
         
 };
+
+//! main */
+int main()
+{
+
+}
     
     
     
