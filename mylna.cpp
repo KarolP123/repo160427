0011564ef9a717b06637548443d0d47e22d5f1fd
KarolP123 <karolpartyka8@wp.pl>
2022-02-04 09:47:05 +0100

    myLna 1.0

---
+++ mylna.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+//! Klasa MyLna */
+class MyLna
+    
+ public:
+{
+    MyLna(double a = 0)  /*!<Metoda klasy MyLna */
+    {
+        
+    }
+    MyLna(double x, double a = 0) /*!<Metoda klasy MyLna */ 
+    {
+        
+    }
+    
+    MyLna(const MyLna &obj) /*!<Metoda klasy MyLna */ 
+    {
+        
+    }
+    ~MyLna() // Destruktor klasy MyLna *!/
+    {
+        
+    }
+    double value(double mX, double mA) /*<! Zwraca wartosc w otoczeniu punktu a */
+    {
+        
+    }
+    void setX(double)
+    {
+        
+    }
+    double getX()
+    {
+        
+    }
+    void setA(double)
+    {
+        
+    }
+    double getA()
+    {
+        
+    }
+    
+private:
+double mX, /*<! double mX */
+double mA  /*<! double mA */
+        
+};
+    
+    
+    
+};
